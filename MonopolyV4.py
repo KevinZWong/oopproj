f3bb0b06a7baa2d31f3612bd213f6602d4e5abfb
KevinZWong <kevinzw8@gmail.com>
2021-01-10 00:02:15 -0800

    changes to V4 file

--- MonopolyV4.py
+++ MonopolyV4.py
@@ -6,7 +6,7 @@ from Players import Players
 from Rent import Rent
 from Property import Property
 import time
-
+'''
 print("Decide the order?, or let the program do it")
 print("1. I'll do it the old fasion way. (roll dice)")
 print("2. let the program do it")
@@ -23,12 +23,14 @@ if decide == 2:
     r.shuffle(PlayerNames)
     print("Order Randomized")
 print("Playernames", PlayerNames)
-
+'''
 
 player = Players()
 rent = Rent()
 property = Property()
 
+
+PlayerNames = ["kevin", "alan", "guo", "jenny", "odi", "pam"]
 
 for i in range(0, len(PlayerNames),1):
     player.addPlayer(PlayerNames[i])
@@ -48,7 +50,7 @@ class runningLoop:
                              "Illinois_Avenue","B._&_O._Railroad","Atlantic_Avenue","Ventnor_Avenue","Water_Works","Marvin_Gardens","Go_To_Jail","Pacific_Avenue",
                              "North_Carolina_Avenue","Community_Chest","Pennsylvania_Avenue", "Short_Line","Chance","Park_Place", "Luxury_Tax","Boardwalk"]
         self.Hotelslist = ["Mediterranean_Avenue","Baltic_Avenue","Reading_Railroad","Oriental_Avenue",
-                             "Vermont_Avenue","Connecticut_Avenue","St._Charles_Place","Eletric_Company","States_Avenue","Virginia_Avenue","Pennsylvania_Railroad",
+                             "Vermont_Avenue","Connecticut_Avenue","St._Charles_Place","Electric_Company","States_Avenue","Virginia_Avenue","Pennsylvania_Railroad",
                              "St._James_Place","Tennessee_Avenue","New_York_Avenue","Kentucky_Avenue","Indiana_Avenue",
                              "Illinois_Avenue","B._&_O._Railroad","Atlantic_Avenue","Ventnor_Avenue","Water_Works","Marvin_Gardens","Pacific_Avenue",
                              "North_Carolina_Avenue","Pennsylvania_Avenue", "Short_Line","Park_Place","Boardwalk"]
@@ -113,35 +115,95 @@ class runningLoop:
             player.updatePosition(loopingList[self.count], self.PositionList[total])
 
             
-            '''
             position = player.getPosition(loopingList[self.count])
             for i in self.Hotelslist:
                 #check it its a hotel and not a chance or a chest
                 if position == i:
                     status = property.getStatus(position)
                     if status == 0:
-                        print("Do you want to buy", player.getPosition(loopingList[self.count]) ,"? : ")
-                        inputbuy = input("Yes, No: ")
-                        inputbuy = inputbuy.upper()
-                        if inputbuy == "Y":
-                            inputbuy = "YES"
-                        if inputbuy == "N": 
-                            inputbuy = "NO"
-                        if inputbuy == "YES":
-                            propertyBuying = player.getPosition(loopingList[self.count])
-                            propPrice = property.getPrice(propertyBuying)
-                            amountP = player.getAmount(loopingList[self.count])
-                            print(amountP)
-                            if amountP >= propPrice:
-                                amountP = amountP - propPrice
-                                print(amountP)
-                                player.setAmount(loopingList[self.count], amountP)
+                        spelling = 0
+                        player.setAmount("kevin", 1)
+                        while(spelling == 0):
+                            print("Do you want to buy", player.getPosition(loopingList[self.count]) ,"? : ")
+                            inputbuy = input("Yes, No: ")
+                            inputbuy = inputbuy.upper()
+                            if inputbuy == "Y":
+                                inputbuy = "YES"
+                            if inputbuy == "N": 
+                                inputbuy = "NO"
+                            if inputbuy == "YES":
+                                propertyBuying = player.getPosition(loopingList[self.count])
+                                propPrice = property.getPrice(propertyBuying)
+                                amountP = player.getAmount(loopingList[self.count])
+                                if amountP >= propPrice:
+                                    amountP = amountP - propPrice
+                                    player.setAmount(loopingList[self.count], amountP)
+                                    player.addProperty(loopingList[self.count], propertyBuying)
+                                    property.setStatus(propertyBuying, 1)
+                                    spelling = 1
+                                else:
+                                    print("1. Get a loan")
+                                    print("2. Sell houses")
+                                    print("3. Morgage a property")
+                                    print("4. Jimmy Deal")
+                                    
+                                    inputMoney = int(input())
+                                    if inputMoney == 1:
+                                        printing = "Who is loaning " + loopingList[self.count] + " money? "
+                                        loaner = input(printing)
+                                        loanAmount = input("Amount Loaning: ")
+                                        loanerMoney = player.getAmount(loaner)
+                                        if not(loanerMoney >= propPrice):
+                                            print(loaner,"does not have enough money to settle",loopingList[self.count],"'s debt.")
+                                            printing = "Would ",loaner, " still like to contribute the rest of his money? Yes/No"
+                                            restofMoney = input(printing)
+                                            restofMoney.upper()
+                                            if restofMoney == "YES":
+                                                loaningMoney = loanerMoney
+                                        if loanerMoney >= propPrice:
+                                            loaningMoney = loanAmount
+                                        Confirm = str(input("Does the loaner confrim to loan his liquid assets? YES/NO "))
+                                        Confirm.upper()
+                                        if Confirm == "YES":
+                                            print("Prociding with transfering assets")
+                                            AfterLoan = loanerMoney - int(loaningMoney)
+                                            player.setAmount(loaner, AfterLoan)
+                                            amountP = amountP + int(loaningMoney)
+                                            player.setAmount(loopingList[self.count], amountP)
+                                            print("It is up to the loaner when the loan is up and the intrest.")
+                                            print("It will be autoatic in the future but man thats gonna be hard.")
+                                    if inputMoney == 2:
+                                        property.setOwner("Mediterranean_Avenue", "kevin")
+                                        property.setHouses("Mediterranean_Avenue", 3)
+                                        property.setOwner("Boardwalk", "kevin")
+                                        property.setHouses("Boardwalk", 3)
+                                        houses = 0
+                                        for i in self.Hotelslist:
+                                            if property.getOwner(i) == loopingList[self.count]:
+                                                houses += property.getHouses(i) 
+
+                                        if houses == 0: 
+                                            print("You have no houses, why would you select this")
+
+                                        else:
+                                            print("All physical Assets")
+                                            for i in self.Hotelslist:
+                                                housesList
+                                                if property.getOwner(i) == loopingList[self.count]:
+                                                    print(i, "has",property.getHouses(i), "houses") 
+                                                    
+
+                                        
+
+
+                            if inputbuy == "NO": 
+                                pass
+                            else:
+                                pass
                     if status == 1:
                         pass
-                            
-            '''
 
-            
+
 
 
 
